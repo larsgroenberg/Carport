f714b255edac29268acb728f8a5c22e85e5d4d2d
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2024-05-02 19:20:35 +0200

    Order-klassen

---
+++ src/main/java/app/entities/Order.java
@@ -0,0 +1,78 @@
+package app.entities;
+
+public class Order {
+    private int orderId;
+    private double materialCost;
+    private double salesPrice;
+    private double carportWidth;
+    private double carportLength;
+    private double carportHeight;
+    private int userId;
+    private String status;
+    private double shedWidth;
+    private double shedLength;
+    private String userEmail;
+
+    public Order(int orderId, double materialCost, double salesPrice, double carportWidth, double carportLength, double carportHeight, int userId, String status, double shedWidth, double shedLength, String userEmail) {
+        this.orderId = orderId;
+        this.materialCost = materialCost;
+        this.salesPrice = salesPrice;
+        this.carportWidth = carportWidth;
+        this.carportLength = carportLength;
+        this.carportHeight = carportHeight;
+        this.userId = userId;
+        this.status = status;
+        this.shedWidth = shedWidth;
+        this.shedLength = shedLength;
+        this.userEmail = userEmail;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public double getMaterialCost() {
+        return materialCost;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public double getCarportWidth() {
+        return carportWidth;
+    }
+
+    public double getCarportLength() {
+        return carportLength;
+    }
+
+    public double getCarportHeight() {
+        return carportHeight;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getShedWidth() {
+        return shedWidth;
+    }
+
+    public double getShedLength() {
+        return shedLength;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
+
