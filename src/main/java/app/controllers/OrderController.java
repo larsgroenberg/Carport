9371cee612a8844de4cd4cf644b40971f0077396
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2024-05-04 11:32:57 +0200

    OrderController.java

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,29 +1,42 @@
 package app.controllers;
 
-import java.io.IOException;
-import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+import app.entities.Carport;
 import app.entities.Order;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
-import app.persistence.OrdersFacade;
+import app.persistence.OrdersMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import app.services.CarportSvg;
 
 public class OrderController {
+    static Carport carport;
+    static Date today = new Date();
+    static SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+    static String formattedDate = formatter.format(today);
     public static void addRoutes(Javalin app) {
         app.get("/", ctx -> {
             ctx.render("index.html");
         });
-        app.post("/createorder", ctx -> {
+        app.post("/createcarport", ctx -> {
             showOrder(ctx);
             ctx.render("showOrder.html");
+        });
+        app.post("/ordercarport", ctx -> {
+            createCarport(ctx, ConnectionPool.getInstance());
+            ctx.render("index.html");
         });
     }
 
     private static void index(Context ctx) {
         ctx.render("adminSite.html");
+    }
+
+    public static Order getOrderByOrderId(int orderId, ConnectionPool connectionPool) throws DatabaseException{
+        return OrdersMapper.getOrderByOrderId(orderId, connectionPool);
     }
 
     public static void showOrder(Context ctx) {
@@ -34,23 +47,33 @@ public class OrderController {
         Locale.setDefault(new Locale("US"));
         CarportSvg svg = new CarportSvg((int) width, (int) length, (int) height);
         ctx.attribute("svg", svg.toString());
+        ctx.sessionAttribute("length", length);
+        ctx.sessionAttribute("width", width);
+        ctx.sessionAttribute("height", height);
     }
 
-    // er klar og testet til når kunden accepterer prisen. Dog skal vi lige sætte skurbredde og højde til default 0 i databasen
-    public static void createCarport(Context ctx, ConnectionPool connectionPool) throws IOException, DatabaseException, SQLException {
+    // er klar og testet
+    public static void createCarport(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        double carportWidth = ctx.sessionAttribute("length");
+        double carportLength = ctx.sessionAttribute("width");
+        double carportHeight = ctx.sessionAttribute("height");
+        // her skal vi sevfølgelig trække på nogle formParam-data fra den kundeindtastningessiden som vi ikke har lavet endnu
         int currentOrderId;
-        int userId = ctx.attribute("userid");
-        double length = Double.parseDouble(ctx.formParam("length"));
-        double width = Double.parseDouble(ctx.formParam("width"));
-        double height = Double.parseDouble(ctx.formParam("height"));
-
-        double shedWidth = Double.parseDouble(ctx.formParam("shedwidth"));
-        double shedLength = Double.parseDouble(ctx.formParam("shedLength"));
+        int userId = 1001;
+        String email = "ole@gmail.com";
+        double shedWidth = 0;
+        double shedLength = 0;
+        String orderDate = formattedDate;
+        //int userId = ctx.attribute("userid");
+        //String email = ctx.formParam("email");
+        //double shedWidth = Double.parseDouble(ctx.formParam("shedwidth"));
+        //double shedLength = Double.parseDouble(ctx.formParam("shedLength"));
 
         try {
-            currentOrderId = OrdersFacade.addOrder(width, length, height, userId, shedWidth, shedLength, ConnectionPool.getInstance());
+            // har ikke sendt sales_price og material_cost med da de skal beregnes her tænker jeg
+            currentOrderId = OrdersMapper.addOrder(carportWidth, carportLength, carportHeight, userId, shedWidth, shedLength, email, orderDate, connectionPool);
             ctx.attribute("current_order_id", currentOrderId);
-            Order order = OrdersFacade.getOrderByOrderId(currentOrderId, ConnectionPool.getInstance());
+            Order order = getOrderByOrderId(currentOrderId, ConnectionPool.getInstance());
             ctx.attribute("length", order.getCarportLength());
             ctx.attribute("width", order.getCarportWidth());
             ctx.attribute("height", order.getCarportHeight());
