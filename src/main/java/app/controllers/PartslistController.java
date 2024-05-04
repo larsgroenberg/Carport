a705e3e86cde85a1a3085ea64632c119d158ede5
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2024-05-04 11:46:12 +0200

    PartslistController.java

--- src/main/java/app/controllers/PartslistController.java
+++ src/main/java/app/controllers/PartslistController.java
@@ -1,26 +1,20 @@
 package app.controllers;
 
-import app.entities.Order;
 import app.entities.Partslistline;
-import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.PartslistMapper;
-import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class PartslistController {
-    static ArrayList<Partslistline> partslistlines = new ArrayList<>();
+    static ArrayList<Partslistline> partslistLines = new ArrayList<>();
 
         public static void addRoutes(Javalin app)
         {
-            app.get("/", ctx -> {
-                ctx.render("index.html");
-            });
+
             app.get("/showpartslist", ctx -> {
                 showPartsList(ctx,ConnectionPool.getInstance());
                 ctx.render("checkoutpage.html");
@@ -34,18 +28,18 @@ public class PartslistController {
 
     public static void showPartsList(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         int orderId = ctx.sessionAttribute("orderid");
-        List<Partslistline> partslist = PartslistMapper.getPartslist(orderId, connectionPool);
+        List<Partslistline> partslist = PartslistMapper.getPartsList(orderId, connectionPool);
         ctx.attribute("partslist", partslist);
     }
 
     public static void insertPartsListLine(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         Partslistline partslistline = ctx.sessionAttribute("partlistline");
         if (ctx.sessionAttribute("partslist") != null) {
-            partslistlines = ctx.sessionAttribute("partslist");
+            partslistLines = ctx.sessionAttribute("partslist");
         }
-        Partslistline newpartslistline = new Partslistline(partslistline.getMaterialId(), partslistline.getOrderId(), partslistline.getQuantity(), partslistline.getPartlistlineprice());
-        partslistlines.add(newpartslistline);
+        Partslistline newpartslistline = new Partslistline(partslistline.getPartId(), partslistline.getOrderId(), partslistline.getQuantity(), partslistline.getPartlistlineprice());
+        partslistLines.add(newpartslistline);
         PartslistMapper.insertPartslistLine(newpartslistline, connectionPool);
-        ctx.attribute("partslist", partslistlines);
+        ctx.attribute("partslist", partslistLines);
     }
 }
