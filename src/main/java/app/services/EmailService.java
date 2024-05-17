73b49cbf3a3475f77aa7b57a0b42f7b34b842b39
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2024-05-17 12:37:42 +0200

    Lavet ny metode i EmailService så admin kan sende email når carport er klar til levering. Har ændret lidt i AdminController for at få den knap der sender emailen til at fungerer.

--- src/main/java/app/services/EmailService.java
+++ src/main/java/app/services/EmailService.java
@@ -1,5 +1,6 @@
 package app.services;
 
+import app.entities.Order;
 import app.entities.User;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
@@ -16,8 +17,42 @@ import java.io.IOException;
  */
 public class EmailService {
 
+    public static void sendCarportReadyEmail(Order orderId) throws IOException {
+        Email from = new Email("auto.mail.sender.service@gmail.com");
+
+        from.setName("Johannes Fog Byggemarked");
+
+        Mail mail = new Mail();
+        mail.setFrom(from);
+
+        String API_KEY = System.getenv("SENDGRID_API_KEY");
+
+        Personalization personalization = new Personalization();
+        String number = "47 16 08 00";
+        personalization.addTo(new Email(orderId.getUserEmail()));
+        personalization.addDynamicTemplateData("phoneNumberForCustomerContact", number);
+        mail.addPersonalization(personalization);
+
+        mail.addCategory("carportapp");
+
+        SendGrid sg = new SendGrid(API_KEY);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+
+            mail.templateId = "d-c3e08a45d7f240d99957f2ae6b8f97c8";
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            System.out.println("Error sending mail");
+            throw ex;
+        }
+    }
     public static void sendEmail(User currentUser) throws IOException {
-        // Erstat xyx@gmail.com med din egen email, som er afsender
         Email from = new Email("auto.mail.sender.service@gmail.com");
 
         from.setName("Johannes Fog Byggemarked");
