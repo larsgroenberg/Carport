12e0f56ebed90de31bf2aafbecdcc70fcf01f7b3
[zPaddyz] <mian19ah@student.cbs.dk>
2024-05-17 11:30:58 +0200

    fjernet kode igen

--- src/main/java/app/persistence/ConnectionPool.java
+++ src/main/java/app/persistence/ConnectionPool.java
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
 public class ConnectionPool {
     // TODO: Change default access credentials for MySql server as needed below:
     private static final String DEFAULT_USER = "postgres";
-    private static final String DEFAULT_PASSWORD = "hsY67kdHtryg43X";
+    private static final String DEFAULT_PASSWORD = System.getenv("Default_Password");
     private static final String DEFAULT_URL = "jdbc:postgresql://161.35.204.41:5432/%s?currentSchema=public";
     private static final String DEFAULT_DB = "carport";
 
