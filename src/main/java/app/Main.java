75bb1c6bb86820ad3b6a6ba481d0d58964a4e661
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2024-10-10 06:06:05 +0200

    en kommentar og har tilføjet en miljøvariabel

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,11 +11,11 @@ public class Main
 {
     public static void main(String[] args) {
 
-        // Initialiserer Javalin og Jetty webserver
+        // Her initialiserer jeg Javalin og Jetty webserver
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-        }).start(7070);
+        }).start(7075);
 
         // Routing
         OrderController.addRoutes(app);
